1c562792d3e77ed33819df69cc3f3ad2c77afb69
Asel <asel@asel.com>
2024-11-08 15:55:18 +0100

    parte de faltas empezada

--- src/views/Faltas.jsx
+++ src/views/Faltas.jsx
@@ -1,10 +1,28 @@
-import { View } from "native-base";
+import { Box, Center, Text, VStack, HStack, View } from "native-base";
+import { FlatList } from "react-native";
+
+const faltas = [
+	{ id: '1', title: 'Falta 1', count: 20 }
+]
 
 const Faltas = () => {
-	return (
-		<View>
+	const renderFaltas = ({ item }) => (
+		<Box flex='1' flexDir='column' bg='red.50'>
+			<HStack flex='1' flexDir='row'>
+				<Text fontSize='lg'>{item.title}</Text>
+				<Text fontSize='lg'>{item.count}</Text>
+			</HStack>
+		</Box>
+	)
 
-		</View>
+	return (
+		<VStack flex={1}>
+			<FlatList
+				data={faltas}
+				renderItem={renderFaltas}
+				keyExtractor={(item) => item.id}
+			/>
+		</VStack>
 	);
 }
 
